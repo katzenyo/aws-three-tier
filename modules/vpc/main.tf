1485ca8b6ac919b1c5855930c38970c4aafbe402
katzenyo <113150671+katzenyo@users.noreply.github.com>
2025-07-31 00:30:21 -0400

    VPC subnetting and route table resources defined

---
+++ modules/vpc/main.tf
@@ -0,0 +1,37 @@
+resource "aws_vpc" "primary" {
+    cidr_block = var.vpc_cidr
+}
+
+resource "aws_subnet" "public" {
+  vpc_id = aws_vpc.primary.id
+  cidr_block = "10.20.0.0/18"
+  map_public_ip_on_launch = true
+  availability_zone = "${var.region}a"
+}
+
+resource "aws_subnet" "private" {
+  vpc_id = aws_vpc.primary.id
+  cidr_block = "10.20.64.0/18"
+  map_public_ip_on_launch = false
+  availability_zone = "${var.region}a"
+}
+
+resource "aws_internet_gateway" "gateway1" {
+  vpc_id = aws_vpc.primary.id
+}
+
+### Route Table Section ####
+### Do not use aws_route resources with aws_route_table + route block, this will cause a race condition
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.primary.id
+
+  route = {
+    cidr_block = "0.0.0.0/0" # full public ingress
+    gateway_id = aws_internet_gateway.gateway1.id
+  }
+}
+
+resource "aws_route_table_association" "public_route" {
+  route_table_id = aws_route_table.public.id
+  gateway_id = aws_internet_gateway.gateway1.id
+}
--- variables.tf
+++ variables.tf
@@ -1,10 +1,11 @@
 variable "region" {
     default = "us-east-1"
+    type = string
 }
 
 variable "ami_id" {
-    description = "Amazon Linux 2 AMI ID for region"
-    default = "tbd"
+    description = "Amazon Linux 2023 AMI 2023.8.20250721.2 x86_64 HVM kernel-6.1"
+    default = "ami-08a6efd148b1f7504"
 }
 
 variable "db_pwd" {
@@ -16,5 +17,11 @@ variable "db_pwd" {
 variable "vpc_cidr" {
   description = "Default VPC CIDR block"
   type = string
-  default = "10.0.0.0/16"
+  default = "10.20.0.0/16"
+}
+
+variable "instance_type" {
+  description = "Free tier instance type"
+  type = string
+  default = "t3.micro"
 }
