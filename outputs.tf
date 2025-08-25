744dd5f27aa7712342389afaca57eba95bd84c21
katzenyo <113150671+katzenyo@users.noreply.github.com>
2025-08-25 01:04:59 -0400

    Added output for the ALB DNS name

--- outputs.tf
+++ outputs.tf
@@ -27,3 +27,8 @@ output "db_endpoint" {
   value = module.rds.database_endpoint
   description = "Postgres endpoint"
 }
+
+output "alb_dns_name" {
+  value = module.alb.alb_dns_name
+  description = "DNS name of the application load balancer"
+}
